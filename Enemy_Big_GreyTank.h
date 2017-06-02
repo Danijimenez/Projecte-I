07cf949226e5a5e330ffe86ee830d4ff255c8efc
Óscar Hernández <oscarhernandezgomez@gmail.com>
2017-06-02 17:11:00 +0200

    Grey Tank

---
+++ Enemy_Big_GreyTank.h
@@ -0,0 +1,25 @@
+#ifndef __Enemy_Big_GreyTank_H__
+#define __Enemy_Big_GreyTank_H__
+
+#include "Enemy.h"
+#include "Path.h"
+
+
+class Big_GreyTank : public Enemy
+{
+private:
+	fPoint original_pos;
+	Path path;
+	bool going_up = true;
+	int original_y = 0;
+	Animation Big_Grey_Tank;
+
+public:
+
+	Big_GreyTank(int x, int y, int path);
+	int life_units = 8;
+	void Move();
+
+};
+
+#endif // __Enemy_Big_GreyTank_H__
