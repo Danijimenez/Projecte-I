47867f18263b5ee62e1c05b3432b011e59096d8e
Óscar Hernández <oscarhernandezgomez@gmail.com>
2017-06-03 20:04:21 +0200

    Missiles

--- Missile.cpp
+++ Missile.cpp
@@ -1,11 +1,19 @@
 #include "Application.h"
+#include "Missile.h"
 #include "ModuleCollision.h"
 #include "ModulePlayer.h"
 
 #include "ModuleInput.h"
 
+Missile::Missile(int x, int y) : Enemy(x, y)
 {
+	nuclear.PushBack({ 534,281,15,13 });
+	nuclear.PushBack({ 554,281,15,13 });
+	nuclear.PushBack({ 574,281,15,13 });
+	nuclear.loop = true;
+	nuclear.speed = 0.1f;
 
+	animation = &nuclear;
 	
 
 	collider = App->collision->AddCollider({ 0, 0, 15, 13 }, COLLIDER_TYPE::COLLIDER_POWER_UP, (Module*)App->enemies);
@@ -21,6 +29,7 @@
 	path.loop = true;
 }
 
+void Missile::Move()
 {
 
 //	original_pos.y -= 1;
--- Missile.h
+++ Missile.h
@@ -1,14 +1,27 @@
+#ifndef __MISSILE_H__
+#define __MISSILE_H__
 
 #include "Enemy.h"
 #include "Path.h"
 
+class Missile : public Enemy
 {
 private:
 
+	fPoint original_pos;
+	Path path;
+	bool going_up = true;
+	int original_y = 0;
+	Animation nuclear;
+	Animation homing;
+
+	
 public:
 
+	Missile(int x, int y);
 
 	void Move();
 };
 
+#endif // __MISSILE_H__
 
